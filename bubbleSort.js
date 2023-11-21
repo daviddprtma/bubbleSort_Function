e05f5d171a56666694629e7283c760863c7d1723
David Pratama <davidchristianpratama@gmail.com>
2023-11-21 11:15:39 +0700

    first commit

---
+++ bubbleSort.js
@@ -0,0 +1,20 @@
+let array = [5,3,6,2,10,1,9,4]
+// implement a bubble sort function
+function bubbleSort(arr) {
+    let swapped;
+    do {
+      swapped = false;
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          let temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+          swapped = true;
+        }
+      }
+    } while (swapped);
+    console.log(arr);
+    return arr;
+} 
+bubbleSort(array); 
+  
